43a2b9aa856700d34dd403e8c57f5391d01abae0
ZehaoChen <a15zehcheche@iam.cat>
2019-06-07 14:15:39 +0200

    Error merge

--- Backend/bbdd/ProductoDao.php
+++ Backend/bbdd/ProductoDao.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace AiMi\Bbdd;
-<<<<<<< HEAD
 
 use AiMi\Models\Producto;
 use AiMi\Models\Categoria;
@@ -78,14 +77,3 @@ class ProductoDao
         return $productos;
     }
 }
-=======
-use AiMi\Models\Producto;
-
-class ProductoDao {
-    public function findAll() 
-    {
-        return Producto::all();
-    }
-}
-?>
->>>>>>> e440577c052fb8426acd5e4503936d09272e5d47
